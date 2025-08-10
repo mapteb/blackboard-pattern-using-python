12b37cfa6b458269b41bc1f76b9e06ff035a149e
Nalla Senthilnathan <nrsenthil@gmail.com>
2025-08-09 21:23:22 -0400

    comments cleanup

--- src/bbpattern/bbcomps/KSInterface.py
+++ src/bbpattern/bbcomps/KSInterface.py
@@ -6,10 +6,6 @@ from bbcomps.Blackboard import Blackboard
 
 
 class KSInterface(ABC):
-
-    # def __init__(self, name):
-    #     super.__init__(self)
-    #     self._name = name
             
     @abstractmethod
     def process(self, bbsubTask: BBSubTask, bb: Blackboard):
